f5002c76acfbe04e8eba523baa733c837c9bcf50
Vlad Kononenko <vlad.kononenko95@gmail.com>
2022-09-23 16:50:54 +0300

    Voximplant iOS SDK 2.46.10 release

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "VoximplantSDK", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/2.46.9/VoximplantSDK_bitcode.zip", checksum: "45e48347bebb6353967ee764ac534a67051bcb711fddaea18b4297f5e5fd9131"),
+        .binaryTarget(name: "VoximplantSDK", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/2.46.10/VoximplantSDK_bitcode.zip", checksum: "960c83f72e2bacf4263d6f04ea7e772fef24cb495a6fcec5f042c7f13aa54b09"),
         .binaryTarget(name: "VoximplantWebRTC", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-webrtc/93.0.0/WebRTC_bitcode.zip", checksum: "e1cf59e381ebb508e94630cd92cf4a46af2bd3021b01b1f3809cde21645dbd7c"),
     ]
 )
